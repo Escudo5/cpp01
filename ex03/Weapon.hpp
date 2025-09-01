c3445460bc7961908e585e564ad4c8e2e01d41be
Escudo5 <sergiomargar03@gmail.com>
2025-09-01 17:02:53 +0200

    empiezo ex03

---
+++ ex03/Weapon.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/01 15:50:05 by smarquez          #+#    #+#             */
+/*   Updated: 2025/09/01 16:36:02 by smarquez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+
+#include <string>
+#include <iostream>
+
+class Weapon
+{
+    public:
+        Weapon(const std::string &type);
+        const std::string &getType()const;
+        std::string setType();
+
+
+    private: 
+        std::string type;
+};
+
+
+
+#endif
+
+
+// **const std::string &getType()const;**
+
+//El primer const → no permite modificar lo que devuelves.
+//El & → devuelves una referencia, no una copia.
+//El const final → asegura que el método no cambia el objeto.
